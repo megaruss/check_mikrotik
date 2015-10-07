f4cef712e5f1758beb39079bb2c1e1c08fcb9189
Russell Tester <russ@mektric.net>
2015-10-07 14:30:30 +1300

    add wireless signal monitoring

--- check_mikrotik.py
+++ check_mikrotik.py
@@ -10,11 +10,12 @@ import rosapi
 logging.basicConfig(level=logging.INFO)
 
 exits = [{"text": "Ok", "code": 0}, {"text": "Warning", "code": 1}, {"text": "Critical", "code": 2}, {"text": "Unknown", "code": 3}]
+exit = exits[3]
 
 parser = argparse.ArgumentParser(description='Check for monitoring Mikrotik Routerboards')
 parser.add_argument('-H', help='Host address')
 parser.add_argument('-p', help='ROS API Port', default=8728)
-parser.add_argument('-m', help='Check type',default="resources")
+parser.add_argument('-t', help='Check type',default="resources")
 parser.add_argument('-U', help='Username', default="admin")
 parser.add_argument('-P', help='Password', default="")
 args = parser.parse_args()
@@ -28,43 +29,80 @@ def gather_info(command):
 	s.connect((args.H, args.p))
 	api = rosapi.RosAPI(s)
 	api.login(args.U, args.P)
-	result = api.talk([command, "" ])
+	result = api.talk([command])
 	return result
 
-res = gather_info("/system/resource/print")[0][1]
+if args.t == "resources": 
 
-values = {}
+	res = gather_info("/system/resource/print")[0][1]
 
-# Calculate memory
-total = float(res['total-memory'])
-free = float(res['free-memory'])
-used = total - free
-values['Memory Used'] = float(round(used / total * 100,2))
+	values = {}
 
-# Calculate HDD Space
-total = float(res['total-hdd-space'])
-free = float(res['free-hdd-space'])
-used = total - free
-values['HDD Used Space'] = int(round(used / total * 100,2))
+	# Calculate memory
+	total = float(res['total-memory'])
+	free = float(res['free-memory'])
+	used = total - free
+	values['Memory Used'] = float(round(used / total * 100,2))
 
-# CPU is easy
-values['CPU Load'] = int(res['cpu-load'])
+	# Calculate HDD Space
+	total = float(res['total-hdd-space'])
+	free = float(res['free-hdd-space'])
+	used = total - free
+	values['HDD Used Space'] = float(round(used / total * 100,2))
 
-info = "Routerboard " + res['board-name'] + " (" + res['architecture-name'] + ") RouterOS " + res['version']  + ", Uptime: " + res['uptime'] 
-message = ""
-perfdata = " | "
-for metric in ["CPU Load", "HDD Used Space", "Memory Used"]:
+	# CPU is easy
+	values['CPU Load'] = float(res['cpu-load'])
 
-	if values[metric] >= critical: 
-		exit = exits[2]
-		message += metric + " above critical threshold of " + str(critical) +"% "
-	elif values[metric] >= warning: 
-		exit = exits[1]
-		message += metric + " above critical threshold of " + str(warning) + "% " 
-	else: 
-		exit = exits[0]
-		message += metric + " is OK (" + str(values[metric]) + "%) "
-	perfdata += metric.lower().replace(" ", "_") + "=" + str(values[metric]) + "%, "
+	info = "Routerboard " + res['board-name'] + " (" + res['architecture-name'] + ") RouterOS " + res['version']  + ", Uptime: " + res['uptime'] 
+	message = ""
+	perfdata = " | "
+	for metric in values:
 
-print exit["text"] + ": " + message  + info + perfdata
+		if values[metric] >= critical: 
+			exit = exits[2]
+			message += metric + " above critical threshold of " + str(critical) +"% "
+		elif values[metric] >= warning: 
+			exit = exits[1]
+			message += metric + " above critical threshold of " + str(warning) + "% " 
+		else: 
+			exit = exits[0]
+			message += metric + " is OK (" + str(values[metric]) + "%) "
+		perfdata += "'" + metric + "'" + "=" + str(values[metric]) + "%;" + str(warning) + ";" + str(critical) + ";; " 
+	print(exit["text"] + ": " + message  + info + perfdata)
+
+elif args.t == "wireless_signal":
+	message = ""
+	perfdata = ""
+	res = gather_info("/interface/wireless/registration-table/print")
+	del res[-1] #remove !done last element 
+	client = dict()
+	for reg in res: 
+		client[reg[1]["mac-address"]] = dict()
+		if "signal-strength-ch0" in reg[1].keys(): 
+			client[reg[1]["mac-address"]]["Signal Chain 0"] = int(reg[1]["signal-strength-ch0"])
+		if "signal-strength-ch1" in reg[1].keys():
+			client[reg[1]["mac-address"]]["Signal Chain 1"] = int(reg[1]["signal-strength-ch1"])
+		if "signal-to-noise" in reg[1].keys():
+			client[reg[1]["mac-address"]]["Signal to Noise"] = int(reg[1]["signal-to-noise"])
+		if "rx-ccq" in reg[1].keys():
+			client[reg[1]["mac-address"]]["RX CCQ"] = int(reg[1]["rx-ccq"])
+		if "tx-ccq" in reg[1].keys():
+			client[reg[1]["mac-address"]]["TX CCQ"] = int(reg[1]["tx-ccq"])
+		if "rx-rate" in reg[1].keys(): 
+			client[reg[1]["mac-address"]]["RX Rate"] = reg[1]["rx-rate"]
+		if "tx-rate" in reg[1].keys():
+			client[reg[1]["mac-address"]]["TX Rate"] = reg[1]["tx-rate"]
+	
+	message = "Found " + str(len(client)) + " wireless clients\n"
+
+	for c in client: 
+		message += "Wireless client: " + c + " - "
+		for metric in client[c]: 
+			message += metric + ": " + str(client[c][metric]) + "; "
+			if type(client[c][metric]) == int: 
+				perfdata += "'" + metric + "'=" + str(abs(client[c][metric])) + ";" + str(warning) + ";" + str(critical) + ";; "
+		message += "\n"
+	print message + perfdata
+	exit = exits[0]
+
 sys.exit(exit["code"])
